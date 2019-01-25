4a0a8beee4724199ae1a467d637e140ff8db3e06
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-24 20:21:02 -0500

    added Scanner Class and experimenting with user input

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 import java.awt.*;
+import java.util.Scanner;
 
 // Java auto runs the main class and main function when it executes a program
 public class Main {
 
     public static void main(String[] args) {
 
-        String userInput = "entertainment";
+
+        //Creating instance of Scanner Class imported above
+        // Scanner knows to look for input in "System.in"
+
+        Scanner sc = new Scanner(System.in);
+
+        // Next will grab the next input from System.in
+        String userInput = sc.next();
         String upperCase = userInput.toUpperCase();
 
         System.out.println(upperCase);
 
-        char firstCharacter = userInput.charAt(0);
-        System.out.println(firstCharacter);
+       char firstCharacter = userInput.charAt(0);
+       System.out.println(firstCharacter);
+
+        System.out.println("Contains:  " + userInput.contains("enter"));
     }
 }
 
