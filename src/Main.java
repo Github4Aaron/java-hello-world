7023f494718b9d4bbeebdfe87566ac34ce3fe458
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-24 22:58:03 -0500

    working with arrays

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
 import java.awt.*;
 import java.util.Scanner;
+import java.util.Arrays;
 
 // Java auto runs the main class and main function when it executes a program
 public class Main {
 
     public static void main(String[] args) {
 
+            //Setting the size of the array, and cannot be changed
+            int[] numbers = new int[5];
 
-        //Creating instance of Scanner Class imported above
-        // Scanner knows to look for input in "System.in"
+            numbers[0] = 31;
+            numbers[1] = 45;
+            numbers[2] = 27;
+            numbers[3] = 43;
+            numbers[4] = 13;
 
-        Scanner sc = new Scanner(System.in);
+            Arrays.sort(numbers);
+            System.out.println(numbers); // This will print out memory address
+            System.out.println(Arrays.toString(numbers));  // This will print out numbers in their order
 
-        // Next will grab the next input from System.in
-        String userInput = sc.next();
-        String upperCase = userInput.toUpperCase();
+            // Below declares, allocates memory, and initializes int
+            int[] numbers2 = {22, 34, 45, 56, 21};
 
-        System.out.println(upperCase);
+            System.out.println(numbers[0]);
+            System.out.println(numbers2[1]);
 
-       char firstCharacter = userInput.charAt(0);
-       System.out.println(firstCharacter);
+            String[] myFavoriteCandyBars = {"twix", "hersheys", "payday", "reeses", "snickers"};
+            System.out.println(myFavoriteCandyBars[3]);
 
-        System.out.println("Contains:  " + userInput.contains("enter"));
     }
 }
 
