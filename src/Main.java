850dc92370a7e607d1e7e877a428f9dfb5919154
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-28 12:25:36 -0500

    added java.util and used Math class and Random util

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import java.awt.*;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.concurrent.Future;
+import java.util.Random;
 
 // Java auto runs the main class and main function when it executes a program
 public class Main {
-
+    // Uses Math class from java.lang library which uses class method .pow (didn't have to create instance)
     public static void main(String[] args) {
+     double power = Math.pow(5, 3);
+     System.out.println(power);
 
-      System.out.println("For Loop");
-      for (int i = 3; i > 0; i --) {
-          System.out.println("Current i " + i);
-      }
-    System.out.println();
+     double squareRoots = Math.sqrt(64);
+     System.out.println(squareRoots);
+
+     Random rand = new Random();
+     int randomNum = rand.nextInt();
+     int randomNumWithBound = rand.nextInt(10);
+        System.out.println(randomNum);
+        System.out.println(randomNumWithBound);
     }
 }
