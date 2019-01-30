4e19d9c9f6a8275a8bf9d228c0bd478f9746abb0
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-30 13:52:42 -0500

    added attribute of previousRoll to Dice class

--- src/Dice.java
+++ src/Dice.java
@@ -1,8 +1,9 @@
 import java.util.Random;
 
 public class Dice {
-    // Instance of Random class is a property
+    // Instance of Random class is a property / attribute of class
     Random rand;
+    int previousRoll = -1;
 
     // Constructor of Dice Instance
     public Dice() {
@@ -12,6 +13,8 @@ public class Dice {
     // Roll method and currentRoll will always be between 1-6
     public int roll() {
         int currentRoll = this.rand.nextInt(6) + 1;
+        // Once currentRoll is returned it becomes the previousRoll
+        this.previousRoll = currentRoll;
         return currentRoll;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,11 @@ public class Main {
     public static void main(String[] args) {
         //Creation of Dice instance
         Dice dice = new Dice();
+        System.out.println("Previous " + dice.previousRoll);
         System.out.println(dice.roll());
+        System.out.println("Previous " + dice.previousRoll);
         System.out.println(dice.roll());
+        System.out.println("Previous " + dice.previousRoll);
     }
 
 }
